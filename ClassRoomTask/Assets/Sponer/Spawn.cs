6867c5438661ad623fe06a43f8f5cb1b246c5498
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2022-11-11 15:05:37 +0900

    スポナー完成。スポナーフォルダを作成、スポナーフォルダの中にスポナープレハブとスポーンスクリプトが入ってる。

--- ClassRoomTask/Assets/Sponer/Spawn.cs
+++ ClassRoomTask/Assets/Sponer/Spawn.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class Spawn : MonoBehaviour
 {
     //時間経過とともにオブジェクトをスポーンさせたい。
-    
+
     //デストロイされないので処理がどんどん重くなっていく、、
     //一定の高さまで行ったら消すようにする。
+
+    public int _spownInterval;
     private float _elapsed;
+    
 
     private void Update()
     {
         _elapsed += Time.deltaTime;
         
-        if(_elapsed > 1)
+        if(_elapsed > _spownInterval)
         {
             _elapsed = 0;
 
