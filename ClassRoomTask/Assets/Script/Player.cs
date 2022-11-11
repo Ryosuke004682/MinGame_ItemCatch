75eb79a85ecf3bd17dd8fcd75c45ff8e518033fe
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2022-11-11 16:06:10 +0900

    player移動を追加

--- ClassRoomTask/Assets/Script/Player.cs
+++ ClassRoomTask/Assets/Script/Player.cs
@@ -6,27 +6,128 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour
 {
-    [Header("Playerの移動パラメーター")]
-    public float _speed = 1.0f;
+   //今回は、コライダーの中に入ったらデストロイしてポイント加算をしたい。
+
+
+    [Header("移動用パラメーター")]
+    public float _moveSpeed;
+    public float groundDragPower;
+
+    //[Header("ジャンプ用パラメーター")]
+    //public float _jumpPower;
+    //public float _CoolDownTime;
+    //public float _aerialManipulation; //空中移動
+    //bool jump;
+    //public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("地面検知用パラメーター")]
+    public float _player_CenterHight;//playerの約中心から地面の高さ
+    public LayerMask isGround;
+    bool grounded;
+
+
+    public Transform _direction;
+
+    float _horizontal;
+    float _vertical;
+
+    Vector3 moveDirection;
 
     Rigidbody _rb;
 
-    private void Awake()
+    private void Start()
     {
         _rb = GetComponent<Rigidbody>();
+        _rb.freezeRotation = true;
     }
 
+    private void Update()
+    {
+        //地面を検知する
+        //検知するには少なからず地面にレイが埋まってなきゃ安定した判定が取れない。
+        //なのでPlayerのCenterから、下方向に0.5の長さのレイに0.2を足す。
+
+        grounded = Physics.Raycast(transform.position, Vector3.down,
+            _player_CenterHight * 0.5f + 0.2f, isGround);
+
+        //もし地面に接地していたい引力を考えたい（摩擦を考えるため）
+
+        if (grounded)
+        {
+            _rb.drag = groundDragPower;
+        }
+        else
+        {
+            _rb.drag = 0;
+        }
+
+
+        MoveInput();
+        SpeedController();
+    }
 
     private void FixedUpdate()
     {
-        var horizontal = Input.GetAxis("Horizontal") * _speed;
-        var vertical = Input.GetAxis("Vertical") * _speed;
-
-        _rb.AddForce(horizontal,0,vertical);
+        MovePlayer();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void MoveInput()
     {
-        //Destroy(other.gameObject);
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        _vertical = Input.GetAxisRaw("Vertical");
     }
+
+    /// <summary>
+    /// プレイヤーの移動方向の計算をする。
+    /// </summary>
+    private void MovePlayer()
+    {
+
+        moveDirection = _direction.forward * _vertical
+                                + _direction.right * _horizontal;
+
+        _rb.AddForce(moveDirection.normalized * _moveSpeed * 10, ForceMode.Force);
+
+      
+    }
+    /// <summary>
+    ///  設定したスピードよりも大きくなっているから制御。
+    /// </summary>
+    private void SpeedController()
+    {
+        Vector3 limitSpeed = new(_rb.velocity.x, 0.0f, _rb.velocity.z);
+
+        //速度のリミットを設定する
+        if (limitSpeed.magnitude > _moveSpeed)
+        {
+            Vector3 limited = limitSpeed.normalized * _moveSpeed;
+            _rb.velocity = new Vector3(limited.x, _rb.velocity.y, limited.z);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+    }
+
+
+
+    /// <summary>
+    /// ジャンプと空中操作について
+    /// </summary>
+    //private void Jump()
+    //{
+    //    _rb.velocity = new Vector3(_rb.velocity.x , 0.0f , _rb.velocity.z);
+
+    //    _rb.AddForce(transform.up * _jumpPower , ForceMode.Impulse);
+    //}
+
+    /// <summary>
+    /// ジャンプの準備
+    /// </summary>
+    //private void ResetJump()
+    //{
+    //    jump = true;
+    //}
 }
+
